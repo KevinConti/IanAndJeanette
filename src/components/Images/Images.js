662086a83125db9ec27f875c05658b098e8e669f
unknown <marketing.KevinConti@gmail.com>
2019-06-15 13:51:29 -0400

    Fixed yarn/npm, added storybook

---
+++ src/components/Images/Images.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Images extends React.Component
+{
+    render(){
+        return (
+            <div>
+                <h1>Image Component</h1>
+            </div> 
+        )
+    }
+}
+
+export default Images;
