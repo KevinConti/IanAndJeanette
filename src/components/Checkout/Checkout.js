432cf77a96b87b71dde375bd628a3c911854d280
unknown <marketing.KevinConti@gmail.com>
2019-06-29 19:59:59 -0400

    Fix checkout button

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,94 +1,94 @@
-import React from "react"
+import React from 'react';
 
 const Checkout = class extends React.Component {
   // Initialise Stripe.js with your publishable key.
   // You can find your key in the Dashboard:
   // https://dashboard.stripe.com/account/apikeys
 
-  constructor()
-  {
+  constructor() {
     super();
-    this.testPublicKey = "pk_test_stGXngvsAJBlw1f5FIDGh86k00bjUUw7NM";
+    this.testPublicKey = 'pk_test_stGXngvsAJBlw1f5FIDGh86k00bjUUw7NM';
     this.state = {
-        "plan":"plan_FGM1SZyKBlCIK6",
-        "donationAmountProps": "hidden mb-4",
-        "buttonText": "Donate"
-    }
+      plan: 'plan_FGM1SZyKBlCIK6',
+      donationAmountProps: 'hidden mb-4',
+      buttonText: 'Donate',
+    };
   }
-  
 
   componentDidMount() {
     this.stripe = window.Stripe(this.testPublicKey, {
-      betas: ["checkout_beta_4"],
-    })
-  }
-
-  getQuantity = (event) => {
-    event.preventDefault();
-    if(this.state.shouldProcessPayment)
-    {
-        this.redirectToCheckout();
-    }
-    else
-    {
-        //Clicking for the first time
-        console.log(event);
-        //Do a sliding animation here
-        this.setState({
-        "buttonText":"Confirm",
-        "donationAmountProps":"my-4",
-        "shouldProcessPayment":true
+      betas: ['checkout_beta_4'],
     });
-    }
   }
 
-  storeQuantity = (event) => {
-      if (event.target.value.substring(0,1) != "$")
-      {
-          event.target.value = "$" + event.target.value;
-      }
-      let valueWithoutDollarSign = event.target.value.substring(1, event.target.value.length)
-      
-      
-      this.setState({"quantity": valueWithoutDollarSign});
-  } 
+  getQuantity = event => {
+    event.preventDefault();
+    if (this.state.shouldProcessPayment) {
+      this.redirectToCheckout();
+    } else {
+      //Clicking for the first time
+      console.log(event);
+      //Do a sliding animation here
+      this.setState({
+        buttonText: 'Confirm',
+        donationAmountProps: 'my-4',
+        shouldProcessPayment: true,
+      });
+    }
+  };
+
+  storeQuantity = event => {
+    if (event.target.value.substring(0, 1) != '$') {
+      event.target.value = '$' + event.target.value;
+    }
+    let valueWithoutDollarSign = event.target.value.substring(
+      1,
+      event.target.value.length
+    );
+
+    this.setState({ quantity: valueWithoutDollarSign });
+  };
 
   async redirectToCheckout() {
-      let parsedQuantity = parseInt(this.state.quantity)
-      if (isNaN(parsedQuantity))
-      {
-          alert("Amount must be a whole number, sorry!");
-          return;
-      }
+    let parsedQuantity = parseInt(this.state.quantity);
+    if (isNaN(parsedQuantity)) {
+      alert('Amount must be a whole number, sorry!');
+      return;
+    }
     const { error } = await this.stripe.redirectToCheckout({
-      items: [{plan: this.state.plan, quantity: parsedQuantity}],
-      successUrl: `/index.html`,
-      cancelUrl: `/index.html`,
-    })
+      items: [{ plan: this.state.plan, quantity: parsedQuantity }],
+      successUrl: `https://ianandjeanette.netlify.com/`,
+      cancelUrl: `https://ianandjeanette.netlify.com/`,
+    });
 
     if (error) {
-      console.warn("Error:", error)
+      console.warn('Error:', error);
     }
   }
 
   render() {
     return (
-    <div className="w-full">
-        
+      <div className="w-full">
         <button
-        className={"bg-red-500 rounded text-2xl text-center w-full max-w-sm font-semibold text-white hover:text-black inline-block py-1 hover:bg-green-500"}
-        onClick={event => this.getQuantity(event)}
-      >
-        {this.state.buttonText}
-      </button>
-      <div className={this.state.donationAmountProps}>
-            <input 
-            onChange={ this.storeQuantity }
-            className="shadow border rounded py-2 px-3 text-gray-700 w-full max-w-sm leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Donation amount" />
+          className={
+            'bg-red-500 rounded text-2xl text-center w-full max-w-sm font-semibold text-white hover:text-black inline-block py-1 hover:bg-green-500'
+          }
+          onClick={event => this.getQuantity(event)}
+        >
+          {this.state.buttonText}
+        </button>
+        <div className={this.state.donationAmountProps}>
+          <input
+            onChange={this.storeQuantity}
+            className="shadow border rounded py-2 px-3 text-gray-700 w-full max-w-sm leading-tight focus:outline-none focus:shadow-outline"
+            id="username"
+            type="text"
+            placeholder="Donation amount"
+          />
         </div>
-    </div>
-    )
+      </div>
+    );
   }
-}
+};
 
-export default Checkout
+export default Checkout;
